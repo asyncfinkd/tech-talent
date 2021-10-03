3e5f78158366bd15ce635bec55244fa98bdc4bb7
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-03 11:38:02 +0400

    Notification

--- src/pages/index/IndexPages.tsx
+++ src/pages/index/IndexPages.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import RegistrationCompanyNotification from "../../components/Notification/RegistrationCompany/RegistrationCompanyNotification";
 import "../../styles/index/indexPages.css";
 
 const IndexPages: React.FC = () => {
   return (
     <>
-      <p>hello</p>
+      <RegistrationCompanyNotification />
     </>
   );
 };
