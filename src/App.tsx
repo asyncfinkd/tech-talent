2510afd1a77eade996b2f8016d16f38ea77a52e6
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-03 11:19:11 +0400

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import IndexPages from "./pages/index/IndexPages";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App: React.FC = () => {
   return (
     <>
-      <IndexPages />
+      <div className="root-0-2-1">
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={IndexPages} />
+          </Switch>
+          <Footer />
+        </BrowserRouter>
+      </div>
     </>
   );
 };
