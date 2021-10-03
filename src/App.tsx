904cdc3b5c8e63e59a4a7d376f615b055cfc2078
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-03 10:30:58 +0400

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import React from "react";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
-function App() {
+const App: React.FC = () => {
   return (
     <>
- <div className="root-0-2-1">
-     <Header />
-    <Footer />
-     </div>
+      <div className="root-0-2-1">
+        <Header />
+        <Footer />
+      </div>
     </>
   );
-}
+};
 
 export default App;
