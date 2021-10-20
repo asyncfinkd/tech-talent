bf13fe04eea409ab498761699643d2666887a841
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-20 22:38:51 +0400

    add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import Footer from "modules/footer";
 import Header from "modules/header";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Header />
+      <Footer wantSponsors={true} />
     </>
   );
 };
