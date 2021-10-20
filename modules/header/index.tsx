7e87fce2625906ab84fcc6c86ee6e1bd82fbbcad
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-20 22:30:33 +0400

    start application build on next

---
+++ modules/header/index.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <>
+      <p>Hello</p>
+    </>
+  );
+}
+
+export default Header;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Header from "modules/header";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Home;
