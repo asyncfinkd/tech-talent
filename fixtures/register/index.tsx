83527c86e48a9cf74e5b62519446238a136e0a99
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-21 00:25:30 +0400

    start fixtures

---
+++ fixtures/register/index.tsx
@@ -0,0 +1,37 @@
+export const interest = [
+  {
+    id: 0,
+    text: "Development",
+    backgroundColor: "rgb(237, 227, 255)",
+    slug: "dev",
+    fill: "rgb(117, 41, 251)",
+  },
+  {
+    id: 1,
+    text: "Design",
+    backgroundColor: "rgb(213, 247, 255)",
+    slug: "design",
+    fill: "rgb(49, 170, 225)",
+  },
+  {
+    id: 2,
+    text: "Marketing",
+    backgroundColor: "rgb(255, 213, 201)",
+    slug: "marketing",
+    fill: "rgb(255, 94, 54)",
+  },
+  {
+    id: 3,
+    text: "Product",
+    backgroundColor: "rgb(237, 227, 255)",
+    slug: "product",
+    fill: "rgb(117, 41, 251)",
+  },
+  {
+    id: 4,
+    text: "Operations",
+    backgroundColor: "rgb(213, 247, 255)",
+    slug: "ops",
+    fill: "rgb(49, 170, 225)",
+  },
+];
---
+++ types/register/firstPage/index.tsx
@@ -0,0 +1,7 @@
+export interface ItemProps {
+  id: number;
+  text: string;
+  backgroundColor: string;
+  slug: string;
+  fill: string;
+}
