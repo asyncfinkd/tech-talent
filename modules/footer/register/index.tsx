1b118296c033c3869d6e977c178dfab2c005d662
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-21 00:42:21 +0400

    split code

---
+++ modules/footer/register/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const RegisterFooter: React.FC = () => {
+  return (
+    <>
+      <div className="placeholder-0-2-235"></div>
+      <div className="footer-0-2-236">
+        <div className="line-0-2-237">
+          <div className="progress-0-2-238" style={{ width: "20%" }}></div>
+        </div>
+        <div className="marginOnMobile-0-2-134">
+          <div className="left-0-2-239">
+            <div className="textGroup-0-2-240">
+              <div className="h1-0-2-241">Tell us who you are</div>
+              <div className="caption-0-2-242">Select profession</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RegisterFooter;
--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -4,6 +4,7 @@ import { interest } from "fixtures/register";
 import Link from "next/link";
 import { ItemProps } from "types/register/firstPage";
 import Head from "next/head";
+import RegisterFooter from "modules/footer/register";
 
 const Register: NextPage = () => {
   const renderLinks = (type: string) => {
@@ -84,20 +85,7 @@ const Register: NextPage = () => {
               </div>
             </div>
           </div>
-          <div className="placeholder-0-2-235"></div>
-          <div className="footer-0-2-236">
-            <div className="line-0-2-237">
-              <div className="progress-0-2-238" style={{ width: "20%" }}></div>
-            </div>
-            <div className="marginOnMobile-0-2-134">
-              <div className="left-0-2-239">
-                <div className="textGroup-0-2-240">
-                  <div className="h1-0-2-241">Tell us who you are</div>
-                  <div className="caption-0-2-242">Select profession</div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <RegisterFooter />
         </div>
       </main>
     </>
