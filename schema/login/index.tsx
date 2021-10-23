a6f580a6d1b5ed50c908de96ae5a1e0b12b6f5e4
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-24 01:15:38 +0400

    admin login validation

--- schema/login/index.tsx
+++ schema/login/index.tsx
@@ -6,11 +6,11 @@ export const schema = yup
     email: yup
       .string()
       .trim()
-      .required("შეიყვანეთ ელექტრონული ფოსტა")
+      .required("Valid email is required")
       .matches(
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "არასწორი ფორმატი"
+        "Valid email is required"
       ),
-    password: yup.string().required("შეიყვანეთ პაროლი"),
+    password: yup.string().required("Password is required"),
   })
   .required();
