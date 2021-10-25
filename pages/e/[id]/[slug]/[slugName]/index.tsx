818908ddefd622a82e43adde70f824c73c59a775
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-10-25 15:13:57 +0400

    add detail page components

--- pages/e/[id]/[slug]/[slugName]/index.tsx
+++ pages/e/[id]/[slug]/[slugName]/index.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import env from "application/environment/env.json";
+import { GetServerSideProps } from "next";
+import jwt_decode from "jwt-decode";
+import EduFeedModule from "modules/e/[id]/[slug]/[slugName]";
 
-function EduFeed({ data }: any) {
+function EduFeed({ responseData, log, token }: any) {
+  const [data, setData] = useState<any>(responseData);
+  const [logged, setLogged] = useState<boolean>(log);
+  const [access_token, setAccess_Token] = useState<any>(token);
+
   return (
     <>
+      {console.log(logged)}
+      {console.log(access_token)}
       {console.log(data)}
-      <p>Hello</p>
+      <EduFeedModule access_token={access_token} logged={logged} data={data} />
     </>
   );
 }
 
-export async function getServerSideProps(context: any) {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   let { slugName } = context.query;
   const requestData = await fetch(`${env.host}/api/get/edu/feed`, {
     method: "POST",
@@ -21,7 +30,27 @@ export async function getServerSideProps(context: any) {
   });
   const responseData = await requestData.json();
 
-  return { props: { data: responseData } };
-}
+  const { req, res } = context;
+  const { cookies } = req;
+  let logged: boolean = false;
+  let token: any = {};
+  if (cookies.cookie) {
+    const request = await fetch(`${env.host}/api/check/logged`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${cookies.cookie}`,
+      },
+      body: JSON.stringify({}),
+    });
+
+    token = jwt_decode(cookies.cookie);
+
+    const response = await request.json();
+    logged = response.success;
+  }
+
+  return { props: { responseData, log: logged, token } };
+};
 
 export default EduFeed;
