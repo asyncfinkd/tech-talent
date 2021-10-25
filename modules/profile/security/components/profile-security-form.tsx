deae4158066bae2219fcfbe4ef95c563f4caf9f6
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-10-25 13:28:26 +0400

    Update profile-security-form.tsx

--- modules/profile/security/components/profile-security-form.tsx
+++ modules/profile/security/components/profile-security-form.tsx
@@ -51,7 +51,7 @@ export default function ProfileSecurityForm({
                   setCurrentPasswordError,
                 },
                 {
-                  onSuccess: (result: Result) => {
+                  onSuccess: () => {
                     setSuccessMessage(true);
                   },
                 }
