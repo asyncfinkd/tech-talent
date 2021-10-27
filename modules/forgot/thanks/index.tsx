b9de52bc42360f1e6d7ec4e60988e93c697da6fb
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-10-27 15:27:33 +0400

    forgot password thanks design

--- modules/forgot/thanks/index.tsx
+++ modules/forgot/thanks/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Footer from "ui/footer";
+import Header from "ui/header";
 
-export default function ForgotThanksModules() {
+export default function ForgotThanksModules({ access_token, logged }: any) {
   return (
     <>
-      <p>Forgot Thanks page</p>
+      <Header ShowShadow={true} access_token={access_token} logged={logged} />
+      <main className="main-0-2-2">
+        <div className="marginOnMobile-0-2-111">
+          <div className="root-0-2-279" style={{ margin: "2rem 0px" }}>
+            <img
+              src="https://tt.ge/images/empty-jobs.png"
+              alt="Password reset process is completed"
+              className="image-0-2-280"
+            />
+            <h1 className="h1-0-2-281">Password reset process is completed</h1>
+            <div className="caption-0-2-282">
+              Check your email and open the password reset link
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer wantSponsors={true} access_token={access_token} logged={logged} />
     </>
   );
 }
--- pages/forgot/thanks/index.tsx
+++ pages/forgot/thanks/index.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import ForgotThanksModules from "modules/forgot/thanks";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import jwt_decode from "jwt-decode";
+import env from "application/environment/env.json";
 
-export default function ForgotThanksPages() {
+export default function ForgotThanksPages({
+  data,
+  token,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [logged, setLogged] = useState<any>(data);
+  const [access_token, setAccess_Token] = useState<any>(token);
+
   return (
     <>
-      <ForgotThanksModules />
+      <ForgotThanksModules access_token={access_token} logged={logged} />
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  const { req, res } = ctx;
+  const { cookies } = req;
+  let logged: boolean = false;
+  let token: any = {};
+  if (cookies.cookie) {
+    const request = await fetch(`${env.host}/api/check/logged`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${cookies.cookie}`,
+      },
+      body: JSON.stringify({}),
+    });
+
+    token = jwt_decode(cookies.cookie);
+
+    const response = await request.json();
+    logged = response.success;
+  }
+
+  return { props: { data: logged, token } };
+};
