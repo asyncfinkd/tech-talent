2d5d9e7d90a4edcd36b08798f4b1e1929cdf1f4a
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-10-27 16:04:45 +0400

    get Types

--- lib/filterDate/index.ts
+++ lib/filterDate/index.ts
@@ -1,8 +1,10 @@
 export function filterDate({ setDate, item }: any): void {
-  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
   let splitDay = item.createdAt.split("-");
-  const firstDate: any = new Date(splitDay[0], splitDay[1] - 1, splitDay[2]);
-  const secondDate: any = new Date();
+  const oneDay: number = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+  const firstDate: Date = new Date(splitDay[0], splitDay[1] - 1, splitDay[2]);
+  const secondDate: Date = new Date();
+
+  // @ts-ignore
   const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
   setDate(diffDays - 1);
 }
