250c0e44f945b7a34953c7ff6881d053279c4006
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-29 01:49:00 +0400

    validation from font

--- baklava/App.tsx
+++ baklava/App.tsx
@@ -2,14 +2,34 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import Header from "./ui/header";
 import { StatusBar } from "expo-status-bar";
+import * as Font from "expo-font";
+import { useState } from "react";
+import AppLoading from "expo-app-loading";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "markpro-bold": require("./assets/fonts/MarkPro-Bold.woff2"),
+  });
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        <Header />
-        <StatusBar style="auto" />
-      </ScrollView>
-    </SafeAreaView>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <SafeAreaView>
+        <ScrollView>
+          <Header />
+          <StatusBar style="auto" />
+        </ScrollView>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
 }
