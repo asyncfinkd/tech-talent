e9b5f2a316222662efd8a25fe5b342cb93c7befe
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-10-28 20:05:52 +0400

    logout module

---
+++ modules/logout/index.tsx
@@ -0,0 +1,17 @@
+import { ApplicationContext } from "context/application/ApplicationContext";
+import { deleteCookie } from "lib/deleteCookie";
+import { useRouter } from "next/router";
+import React, { useCallback, useContext } from "react";
+
+export default function Logout() {
+  const router = useRouter();
+  const { setAccess_Token } = useContext(ApplicationContext);
+
+  const removeToken = useCallback(() => {
+    setAccess_Token("");
+    deleteCookie("cookie", "/", "localhost");
+    router.push("/");
+  }, []);
+
+  return <>{removeToken()}</>;
+}
--- pages/logout/index.tsx
+++ pages/logout/index.tsx
@@ -1,17 +1,10 @@
-import { ApplicationContext } from "context/application/ApplicationContext";
-import { deleteCookie } from "lib/deleteCookie";
-import { useRouter } from "next/router";
-import { useCallback, useContext } from "react";
+import Logout from "modules/logout";
+import React from "react";
 
-export default function LogoutPage() {
-  const router = useRouter();
-  const { setAccess_Token } = useContext(ApplicationContext);
-
-  const removeToken = useCallback(() => {
-    setAccess_Token("");
-    deleteCookie("cookie", "/", "localhost");
-    router.push("/");
-  }, []);
-
-  return <>{removeToken()}</>;
+export default function LogoutPages() {
+  return (
+    <>
+      <Logout />
+    </>
+  );
 }
