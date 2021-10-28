a887d891214e7fd2277071377c33af302ea92814
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-29 00:30:02 +0400

    header in baklava

--- baklava/App.tsx
+++ baklava/App.tsx
@@ -1,23 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Header from "./ui/header";
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <SafeAreaView>
+      <Header />
+    </SafeAreaView>
+  );
 }
