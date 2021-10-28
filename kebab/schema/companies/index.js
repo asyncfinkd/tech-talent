5b01ff7c3da4064ef55a86a5b226ef7cc0634d91
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-28 22:35:02 +0400

    fix: server files

---
+++ kebab/schema/companies/index.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const Tags = mongoose.Schema({
+  text: { type: String },
+});
+
+const FollowedUsersId = mongoose.Schema({
+  id: { type: String },
+});
+
+const companiesSchema = mongoose.Schema({
+  about: { type: String },
+  createdAt: { type: String },
+  employees: { type: String },
+  founded: { type: String },
+  hq: { type: String },
+  industry: { type: String },
+  jobCount: { type: Number },
+  logoUrl: { type: String },
+  notifyEmails: { type: String },
+  objective: { type: String },
+  phone: { type: String },
+  remoteFriendly: { type: Boolean },
+  state: { type: String },
+  tags: [Tags],
+  url: { type: String },
+  slug: { type: String },
+  followedUsersId: [FollowedUsersId],
+  backgroundColor: { type: String },
+  primaryText: { type: String },
+  primaryTextColor: { type: String },
+  secondaryText: { type: String },
+  secondaryTextColor: { type: String },
+});
+
+const Companies = mongoose.model("companies", companiesSchema);
+
+module.exports = Companies;
