61f6d8284d174588a41347e95a195da8de13de54
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-10-29 13:24:06 +0400

    carousel

--- baklava/ui/carousel/partners/index.tsx
+++ baklava/ui/carousel/partners/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 import Carousel from "react-native-snap-carousel";
 
 export default function CarouselPartners() {
@@ -14,19 +14,41 @@ export default function CarouselPartners() {
 
   const renderItem = ({ item }: any) => {
     return (
-      <View>
-        <Text>{item.image}</Text>
+      <View style={styles.carouselItemContainer}>
+        <Image
+          source={{ uri: `${item.image}` }}
+          style={styles.carouselImageContainer}
+        />
       </View>
     );
   };
   return (
     <>
-      <Carousel
-        data={carouselItems}
-        sliderWidth={300}
-        itemWidth={300}
-        renderItem={renderItem}
-      />
+      <View style={{ display: "flex", alignItems: "center" }}>
+        <Carousel
+          data={carouselItems}
+          sliderWidth={300}
+          itemWidth={300}
+          renderItem={renderItem}
+        />
+      </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  carouselItemContainer: {
+    borderColor: "red",
+    borderWidth: 1,
+    padding: 20,
+    display: "flex",
+    alignItems: "center",
+  },
+  carouselImageContainer: {
+    width: "100%",
+    height: 70,
+    display: "flex",
+    resizeMode: "contain",
+    alignItems: "center",
+  },
+});
