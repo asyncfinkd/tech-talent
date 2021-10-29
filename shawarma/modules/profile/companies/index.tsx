ec2949aae38d3e29690b72f79d0a5080361d59b1
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-29 23:47:30 +0400

    Head in Profile Companies Page

--- shawarma/modules/profile/companies/index.tsx
+++ shawarma/modules/profile/companies/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import Footer from "ui/footer";
 import Header from "ui/header";
@@ -5,6 +6,9 @@ import Header from "ui/header";
 export default function ProfileCompaniesModule({ access_token, logged }: any) {
   return (
     <>
+      <Head>
+        <title>followed | User | Tech Talent</title>
+      </Head>
       <Header ShowShadow={true} access_token={access_token} logged={logged} />
 
       <Footer wantSponsors={true} access_token={access_token} logged={logged} />
