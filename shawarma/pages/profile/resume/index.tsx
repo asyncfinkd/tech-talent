aa136b2edeb0155a58f10f861747dedc1a41da06
asyncfinkd <shamgoingzuck@gmail.com>
2021-10-30 13:40:40 +0400

    import auths for development token

--- shawarma/pages/profile/resume/index.tsx
+++ shawarma/pages/profile/resume/index.tsx
@@ -1,9 +1,26 @@
+import { useAuth } from "lib/useAuth";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
 import React from "react";
 
-export default function ProfileResumePage() {
+function ProfileResumePage({
+  data,
+  token,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
       <p>Hello</p>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  return useAuth({ ctx, wantAuthentication: true });
+};
+
+export default ProfileResumePage;
