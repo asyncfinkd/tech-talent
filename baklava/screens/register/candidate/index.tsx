32b89a948a75a4c578ec11cbc0ab9a5d0fe4d4fa
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-01 13:42:50 +0400

    Update index.tsx

--- baklava/screens/register/candidate/index.tsx
+++ baklava/screens/register/candidate/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Text } from "react-native";
 import RegisterCandidateModules from "../../../modules/register/candidate";
 
 export default function RegisterCandidateScreen({ navigation }: any) {
