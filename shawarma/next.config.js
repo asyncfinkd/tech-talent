8acb300c9d76c762c2aa651185206c2023bb13b0
asyncfinkd <shamgoingzuck@gmail.com>
2021-11-02 00:57:12 +0400

    for host

--- shawarma/next.config.js
+++ shawarma/next.config.js
@@ -8,4 +8,8 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return config;
+  },
 };
