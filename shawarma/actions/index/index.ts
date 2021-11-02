634e44c109b82d1444d20e71ccf9d512816de02e
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-02 13:56:46 +0400

    start dev by actions

---
+++ shawarma/actions/index/index.ts
@@ -0,0 +1,9 @@
+import Footer from "ui/footer";
+import Header from "ui/header";
+import Notification from "ui/notification";
+
+export default {
+  Footer,
+  Header,
+  Notification,
+};
--- shawarma/modules/index/IndexPage.tsx
+++ shawarma/modules/index/IndexPage.tsx
@@ -1,15 +1,21 @@
-import Footer from "ui/footer";
-import Header from "ui/header";
+import Actions from "actions/index";
 import type { NextPage } from "next";
-import Notification from "ui/notification";
 import React from "react";
 
 const Home: NextPage<any> = ({ logged, access_token }: any) => {
   return (
     <>
-      <Header access_token={access_token} logged={logged} ShowShadow={true} />
-      <Notification />
-      <Footer logged={logged} access_token={access_token} wantSponsors={true} />
+      <Actions.Header
+        access_token={access_token}
+        logged={logged}
+        ShowShadow={true}
+      />
+      <Actions.Notification />
+      <Actions.Footer
+        logged={logged}
+        access_token={access_token}
+        wantSponsors={true}
+      />
     </>
   );
 };
