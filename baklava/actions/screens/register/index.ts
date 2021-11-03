3ac1724119dde4d71475cf57b6271fe9cb259642
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-03 14:55:15 +0400

    add new actions

---
+++ baklava/actions/screens/register/index.ts
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+import RegisterPageModules from "../../../modules/register";
+import RegisterFooter from "../../../ui/footer/register";
+
+export default {
+  View,
+  RegisterPageModules,
+  RegisterFooter,
+};
--- baklava/screens/register/index.tsx
+++ baklava/screens/register/index.tsx
@@ -1,15 +1,13 @@
+import Actions from "../../actions/screens/register";
 import React from "react";
-import { Text, View } from "react-native";
-import RegisterPageModules from "../../modules/register";
-import RegisterFooter from "../../ui/footer/register";
 
 export default function RegisterScreen({ navigation }: any) {
   return (
     <>
-      <View style={{ flex: 1 }}>
-        <RegisterPageModules navigation={navigation} />
-        <RegisterFooter firstStep={true} />
-      </View>
+      <Actions.View style={{ flex: 1 }}>
+        <Actions.RegisterPageModules navigation={navigation} />
+        <Actions.RegisterFooter firstStep={true} />
+      </Actions.View>
     </>
   );
 }
