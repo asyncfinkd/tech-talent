8e8f5dca197e05f6b407198cda396d6b1e0768e8
asyncfinkd <shamgoingzuck@gmail.com>
2021-11-09 09:46:26 +0400

    fix loop error

--- shawarma/pages/manager/register/info/index.tsx
+++ shawarma/pages/manager/register/info/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CryptoAES from "crypto-js/aes";
 import CryptoENC from "crypto-js/enc-utf8";
 import { isServer } from "lib/isServer";
@@ -6,21 +6,29 @@ import { useState } from "react";
 
 export default function ManagerRegisterInfoPages() {
   const [decryptedURL, setDecryptedURL] = useState({});
-  if (!isServer) {
-    let cipherText = window.location.href.split("??")[1];
-    let decryptText = CryptoAES.decrypt(
-      cipherText,
-      "lm3k123kl123kNJ#!#NJ!K#N!@K#Mxnamnx"
-    );
-    let decryptedAlready = decryptText.toString(CryptoENC);
-    let decryptBySplit = decryptedAlready.split("?").join("=").split("=");
-    setDecryptedURL({
-      email: decryptBySplit[1],
-      password: decryptBySplit[3],
-    });
-  }
+
+  const decryptURL = () => {
+    if (!isServer) {
+      let cipherText = window.location.href.split("??")[1];
+      let decryptText = CryptoAES.decrypt(
+        cipherText,
+        "lm3k123kl123kNJ#!#NJ!K#N!@K#Mxnamnx"
+      );
+      let decryptedAlready = decryptText.toString(CryptoENC);
+      let decryptBySplit = decryptedAlready.split("?").join("=").split("=");
+      setDecryptedURL({
+        email: decryptBySplit[1],
+        password: decryptBySplit[3],
+      });
+    }
+  };
+
+  useEffect(() => {
+    decryptURL();
+  }, []);
   return (
     <>
+    {console.log(decryptedURL)}
       <p>Hello</p>
     </>
   );
