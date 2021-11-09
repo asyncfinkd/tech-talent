4bc744632507e2147f720dc7923e5675eaa4f304
asyncfinkd <shamgoingzuck@gmail.com>
2021-11-10 00:30:49 +0400

    get dynamically register fields

---
+++ shawarma/fixtures/register/candidate/info/index.ts
@@ -0,0 +1,19 @@
+export const Form = {
+  Fields: [
+    {
+      name: "fullName",
+      type: "text",
+      title: "First Name & Last Name",
+    },
+    {
+      name: "phone",
+      type: "text",
+      title: "Phone",
+    },
+    {
+      name: "socialNetwork",
+      type: "text",
+      title: "LinkedIn/Dribbble/Behance",
+    },
+  ],
+};
--- shawarma/modules/register/candidate/info/components/register-candidate-info-form.tsx
+++ shawarma/modules/register/candidate/info/components/register-candidate-info-form.tsx
@@ -1,44 +1,32 @@
+import { Form } from "fixtures/register/candidate/info";
 import React from "react";
 
 const RegisterCandidateInfoForm: React.FC<any> = ({ register }: any) => {
   return (
     <>
       <div className="inputGroup-0-2-317">
-        <div className="root-0-2-250 input-0-2-318">
-          <label className="label-0-2-251">
-            First Name & Last Name{" "}
-            <span className="asteriskValid-0-2-252" style={{ display: "none" }}>
-              *
-            </span>
-          </label>
-          <input
-            type="text"
-            className="input-0-2-254"
-            {...register("fullName")}
-          />
-        </div>
-        <div className="root-0-2-250 input-0-2-318">
-          <label className="label-0-2-251">
-            Phone{" "}
-            <span className="asteriskValid-0-2-252" style={{ display: "none" }}>
-              *
-            </span>
-          </label>
-          <input type="text" className="input-0-2-254" {...register("phone")} />
-        </div>
-        <div className="root-0-2-250 input-0-2-318">
-          <label className="label-0-2-251">
-            LinkedIn/Dribbble/Behance{" "}
-            <span className="asteriskValid-0-2-252" style={{ display: "none" }}>
-              *
-            </span>
-          </label>
-          <input
-            type="text"
-            className="input-0-2-254"
-            {...register("socialNetwork")}
-          />
-        </div>
+        {Form.Fields.map((item: any) => {
+          return (
+            <>
+              <div className="root-0-2-250 input-0-2-318">
+                <label className="label-0-2-251">
+                  {item.title}{" "}
+                  <span
+                    className="asteriskValid-0-2-252"
+                    style={{ display: "none" }}
+                  >
+                    *
+                  </span>
+                </label>
+                <input
+                  type={item.type}
+                  className="input-0-2-254"
+                  {...register(item.name)}
+                />
+              </div>
+            </>
+          );
+        })}
       </div>
     </>
   );
