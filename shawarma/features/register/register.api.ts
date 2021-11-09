8d0230578200a66ab50cabbef22ab0a660f78810
asyncfinkd <shamgoingzuck@gmail.com>
2021-11-10 00:24:12 +0400

    delete unused code

--- shawarma/features/register/register.api.ts
+++ shawarma/features/register/register.api.ts
@@ -1,37 +1,4 @@
 import { request } from "api";
 
-// export const RegisterRequest = async (
-//   loginData: {
-//     email: string;
-//     password: string;
-//   },
-//   interest: string | string[] | undefined,
-//   setErrorMessage: React.Dispatch<React.SetStateAction<boolean>>
-// ): Promise<Result> => {
-//   const { email, password } = loginData;
-//   const response = await fetch(
-//     `${process.env.REACT_APP_API_URL}/api/register`,
-//     {
-//       method: "POST",
-//       headers: { "Content-type": "application/json" },
-//       body: JSON.stringify({ email, password, role: "member", interest }),
-//     }
-//   );
-//   if (response.ok) setErrorMessage(false);
-
-//   if (response.ok) {
-//     return response.json();
-//   } else {
-//     const error = await response.json();
-//     if (error) {
-//       setErrorMessage(true);
-//     }
-
-//     return Promise.reject({
-//       ...error,
-//     });
-//   }
-// };
-
 export const RegisterRequest = async ({ loginData, setError }: any) =>
   request("/api/register", "POST", "JSON", loginData, setError);
