58fe1de882d3b9c127b5b0b5a616555bdbc80808
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-12 15:00:50 +0400

    add new features

--- shawarma/pages/manager/branding/index.tsx
+++ shawarma/pages/manager/branding/index.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import { useAuth } from "lib/use-auth";
+import ManagerBrandingModules from "modules/manager/branding";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import React, { useState } from "react";
+import { DecodedAccess_Token } from "types/global";
 
-function ManagerBrandingPages() {
+function ManagerBrandingPages({
+  data,
+  token,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [logged, setLogged] = useState<boolean>(data);
+  const [access_token, setAccess_Token] = useState<DecodedAccess_Token>(token);
+
   return (
     <>
-      <p>Hello</p>
+      <ManagerBrandingModules
+        setAccess_Token={setAccess_Token}
+        access_token={access_token}
+        logged={logged}
+      />
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  return useAuth({ ctx, wantAuthentication: true });
+};
 
 export default ManagerBrandingPages;
