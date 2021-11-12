1b596a5266ded7f0e87a214d51ee6294d8984503
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-12 18:59:55 +0400

    mongodb connect

--- khachapuri/src/app.module.ts
+++ khachapuri/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://giga:vivomini@rest.nl9di.mongodb.net/techtalent?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
