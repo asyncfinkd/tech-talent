a912036aec2b1a4842aa5c835fa99285bd7c53ce
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-19 14:57:34 +0400

    add new configs

--- khachapuri/src/main.ts
+++ khachapuri/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import * as express from 'express';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      cors: true
+    }
+  );
+
+  process.env.PWD = process.cwd();
+  app.use('/public/images', express.static(process.env.PWD + '/public/images'));
+
+  app.use(compression());
+  // app.use(helmet());
+
+  await app.listen(process.env.PORT, () => {
+    console.log(`server is already started ${process.env.PORT}`);
+  });
 }
+
 bootstrap();
